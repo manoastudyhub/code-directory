01727173ab78afc7ad5ba6bb006b7e628b037cec
maxdeyo <maxdeyo@hawaii.edu>
2019-04-02 22:04:39 -1000

    issue-3 commit 2

--- app/imports/ui/pages/UserHome.jsx
+++ app/imports/ui/pages/UserHome.jsx
@@ -23,38 +23,36 @@ export default class UserHome extends React.Component{
           <Menu.Item>Create Study Sesh</Menu.Item>
           <Menu.Item>Study Sesh's</Menu.Item>
           <Menu.Item>Calendar</Menu.Item>
-        </Menu>
+        </Menu>     
         
-        <Grid centered columns="three">
-          <Grid.Column>
-            <Container centered>
-            <Image src='https://react.semantic-ui.com/images/avatar/large/matthew.png' size='medium' />
-            <h2 textAlign="center">Name</h2>
-            </Container>
-          </Grid.Column>
-          <Grid.Column>
-            <Container inverted>
-              <h2>Major</h2>
-              <p>Computer Science</p>
-              <h2>Courses Taken</h2>
-              <List bulleted>
-                <List.Item><Icon name='computer' /> ICS 111</List.Item>
-                <List.Item><Icon name='computer' /> ICS 211</List.Item>
-                <List.Item><Icon name='computer' /> ICS 212</List.Item>
-                <List.Item><Icon name='computer' /> ICS 141</List.Item>
-                <List.Item><Icon name='computer' /> ICS 241</List.Item>
-              </List>
-              <h2>Current Courses</h2>
-              <List bulleted>
-                <List.Item><Icon name='computer' /> ICS 314</List.Item>
-                <List.Item><Icon name='computer' /> ICS 311</List.Item>
-              </List>
-              <h2>Time Available</h2>
-              <p>Monday, Wednesday and Friday after 2pm</p>
-            </Container>
-          </Grid.Column>
-          
-        </Grid>
+          <Grid centered columns={3}>
+            <Grid.Column textAlign="center">
+              <Icon name="user" size="huge"/>
+              <Header as="h2">My Profile</Header>
+            </Grid.Column>
+            <Grid.Column textAlign="center">
+              <Icon name="calendar alternate" size="huge" />
+              <Header as="h2">My Calendar</Header>
+            </Grid.Column>
+            <Grid.Column textAlign="center">
+              <Icon name="user" size="huge" />
+              <Header as="h2">My Study Sesh's</Header>
+            </Grid.Column>
+          </Grid>
+          <Header as="h2">Upcoming Study Sesh's</Header>
+          <List>
+            <List.Item><a>Sinclair 4pm</a></List.Item>
+            <List.Item><a>Hamilton 5pm</a></List.Item>
+            <List.Item><a>Keller 12pm</a></List.Item>
+          </List>
+          <Header as="h2">My Tutors</Header>
+          <List>
+            <List.Item><Image src='https://react.semantic-ui.com/images/avatar/large/matthew.png' size='tiny' />Erica Smith</List.Item>
+            <List.Item><Image src='https://react.semantic-ui.com/images/avatar/large/matthew.png' size='tiny' />Tom Smith</List.Item>
+            <List.Item><Image src='https://react.semantic-ui.com/images/avatar/large/matthew.png' size='tiny' />Erica Smith</List.Item>
+
+
+          </List>
       </div>
     );
   }
