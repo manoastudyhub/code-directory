b2eda506f0f6f653cef22ab543ca4df90ffb0415
tyy21 <tyy21@hawaii.edu>
2019-04-02 23:05:34 -1000

    User Profile Mockup - 2
    Changed Navbar header, picture, name, classes

--- app/imports/ui/pages/UserProfile.jsx
+++ app/imports/ui/pages/UserProfile.jsx
@@ -8,25 +8,25 @@ class UserProfile extends React.Component {
         <Grid verticalAlign='middle' textAlign='center' container>
           <Grid.Row>
             <Grid.Column width={8}>
-              <h1>Your Name Here</h1>
+              <h1>Ty Yamasaki</h1>
             </Grid.Column>
             <Grid.Column width={4}>
-              <Image size='small' square src="/images/meteor-logo.png"/>
+              <Image size='small' square src='https://react.semantic-ui.com/images/avatar/large/steve.jpg'/>
             </Grid.Column>
           </Grid.Row>
           <Grid.Row>
             <Grid.Column width={8}>
               <h2>Current Courses</h2>
               <List>
-                <List.Item>Class</List.Item>
-                <List.Item>Class</List.Item>
-                <List.Item>Class</List.Item>
+                <List.Item>ICS 314</List.Item>
+                <List.Item>ICS 311</List.Item>
+                <List.Item>ICS 241</List.Item>
               </List>
               <h2>Courses Taken</h2>
               <List>
-                <List.Item>Class</List.Item>
-                <List.Item>Class</List.Item>
-                <List.Item>Class</List.Item>
+                <List.Item>ICS 111</List.Item>
+                <List.Item>ICS 211</List.Item>
+                <List.Item>ICS 212</List.Item>
               </List>
             </Grid.Column>
           </Grid.Row>
