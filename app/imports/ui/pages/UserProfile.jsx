d7d3728a131da2b62a0757aae090aaccc29f7df6
tyy21 <tyy21@hawaii.edu>
2019-04-15 12:41:52 -1000

    incomplete1

--- app/imports/ui/pages/UserProfile.jsx
+++ app/imports/ui/pages/UserProfile.jsx
@@ -1,33 +1,32 @@
 import React from 'react';
-import { Grid, Image, List } from 'semantic-ui-react';
+import { Students } from '../../api/student/student';
+import { Grid, Image, Loader } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
+import { withTracker } from 'meteor/react-meteor-data';
+import { Meteor } from 'meteor/meteor';
+
 
 /** A simple static component to render some text for the landing page. */
 class UserProfile extends React.Component {
+
   render() {
+    return (this.props.ready) ? this.renderPage() : <Loader active>Getting data</Loader>;
+  }
+
+  renderPage() {
     return (
         <Grid verticalAlign='middle' textAlign='center' container>
           <Grid.Row>
             <Grid.Column width={8}>
-              <h1>Ty Yamasaki</h1>
+              <h1>{this.props.student.firstName}{this.props.student.lastName}</h1>
             </Grid.Column>
             <Grid.Column width={4}>
-              <Image size='small' square src='https://react.semantic-ui.com/images/avatar/large/steve.jpg'/>
+              <Image size='small' square src={this.props.student.image}/>
             </Grid.Column>
           </Grid.Row>
           <Grid.Row>
             <Grid.Column width={8}>
-              <h2>Current Courses</h2>
-              <List>
-                <List.Item>ICS 314</List.Item>
-                <List.Item>ICS 311</List.Item>
-                <List.Item>ICS 241</List.Item>
-              </List>
-              <h2>Courses Taken</h2>
-              <List>
-                <List.Item>ICS 111</List.Item>
-                <List.Item>ICS 211</List.Item>
-                <List.Item>ICS 212</List.Item>
-              </List>
+
             </Grid.Column>
           </Grid.Row>
         </Grid>
@@ -35,4 +34,18 @@ class UserProfile extends React.Component {
   }
 }
 
-export default UserProfile;
+/** Require an array of Student documents in the props. */
+UserProfile.propTypes = {
+  student: PropTypes.array.isRequired,
+  ready: PropTypes.bool.isRequired,
+};
+
+/** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
+export default withTracker(() => {
+  // Get access to Student documents.
+  const subscription = Meteor.subscribe('Students');
+  return {
+    student: Students.find({}).fetch(),
+    ready: (subscription.ready()),
+  };
+})(UserProfile);
