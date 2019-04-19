867c551a8a3dbcedee3f2d7436173e63df80f84f
Angeli Amascual <aamascua@hawaii.edu>
2019-04-19 12:13:08 -1000

    calendar now exists on page

--- app/imports/ui/pages/Calendar.jsx
+++ app/imports/ui/pages/Calendar.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import { Grid } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class Calendar extends React.Component {
   render() {
+    const calendarSize = { height: '1000px', paddingTop: '100px' };
     return (
-        <div className="manoastudyhub-landing-background">
+        <div className="manoastudyhub-calendar-background">
+          <Grid container verticalAlign="middle" style={calendarSize}>
+          <FullCalendar defaultView="dayGridMonth" plugins={[dayGridPlugin]} />
+          </Grid>
         </div>
     );
   }
