3ba32f502a3f27487d05c46dfc4ab3408afb4261
maxdeyo <maxdeyo@hawaii.edu>
2019-04-24 00:33:31 -1000

    Added registration form, need to fix subjects array

--- app/imports/ui/pages/Signup.jsx
+++ app/imports/ui/pages/Signup.jsx
@@ -1,20 +1,39 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Container, Form, Grid, Header, Message, Segment } from 'semantic-ui-react';
+import { Container, Form, Grid, Header, Message, Segment, Button, Label } from 'semantic-ui-react';
 import { Accounts } from 'meteor/accounts-base';
 
 /**
  * Signup component is similar to signin component, but we attempt to create a new user instead.
  */
+
+const classStandingOptions = [
+  { key: 'freshman', text: 'Freshman', value: 'freshman' },
+  { key: 'sophomore', text: 'Sophomore', value: 'sophomore' },
+  { key: 'junior', text: 'Junior', value: 'junior' },
+  { key: 'senior', text: 'Senior', value: 'senior' },
+  
+]
 export default class Signup extends React.Component {
   /** Initialize state fields. */
   constructor(props) {
     super(props);
-    this.state = { email: '', password: '', error: '' };
+    this.state = { 
+      email: '', 
+      password: '', 
+      firstName: '',
+      lastName: '',
+      classStanding: '',
+      image: '',
+      major: '',
+      subjects: [],
+      error: '' 
+    };
     // Ensure that 'this' is bound to this component in these two functions.
     // https://medium.freecodecamp.org/react-binding-patterns-5-approaches-for-handling-this-92c651b5af56
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.handleSubjectsClick = this.handleSubjectsClick.bind(this);
   }
 
   /** Update the form controls each time the user interacts with them. */
@@ -24,14 +43,23 @@ export default class Signup extends React.Component {
 
   /** Handle Signup submission using Meteor's account mechanism. */
   handleSubmit() {
-    const { email, password } = this.state;
-    Accounts.createUser({ email, username: email, password }, (err) => {
+    const { email, password, firstName, lastName, classStanding, image, major, subjects } = this.state;
+    Accounts.createUser({ email, username: email, password, 
+      profile:{firstName: firstName, lastName: lastName, classStanding: classStanding, image: image, major: major, subjects: subjects } 
+      }, (err) => {
       if (err) {
         this.setState({ error: err.reason });
       } else {
         // browserHistory.push('/login');
+
       }
     });
+  }
+
+  handleSubjectsClick(e, { name, value }){
+    let prevState = this.name.subjects.slice();
+    prevState.push(value);
+    this.setState({[name]: prevState});
   }
 
   /** Display the signup form. */
@@ -64,6 +92,73 @@ export default class Signup extends React.Component {
                       type="password"
                       onChange={this.handleChange}
                   />
+                  <Grid columns={2}>
+                    <Grid.Column>
+                    <Form.Input
+                        label="First Name"
+                        icon="user"
+                        iconPosition="left"
+                        name="firstName"
+                        placeholder="John"
+                        type="firstName"
+                        onChange={this.handleChange}
+                    />
+                    </Grid.Column>
+                    <Grid.Column>
+                    <Form.Input
+                        label="Last Name"
+                        icon="user"
+                        iconPosition="left"
+                        name="lastName"
+                        placeholder="Foo"
+                        type="lastName"
+                        onChange={this.handleChange}
+                    />
+                    </Grid.Column>
+                  </Grid>
+                  <Form.Select 
+                      label="Class Standing"
+                      options={classStandingOptions}
+                      placeholder="Class Standing"
+                      name="classStanding"
+                      type="classStanding"
+                      onChange={this.handleChange}
+                  />
+                  <Form.Input
+                        label="Image"
+                        icon="image"
+                        iconPosition="left"
+                        name="image"
+                        placeholder="Image"
+                        type=""
+                        onChange={this.handleChange}
+                  />
+                  <Form.Input
+                        label="Major"
+                        icon="book"
+                        iconPosition="left"
+                        name="major"
+                        placeholder="Computer Science"
+                        type="major"
+                        onChange={this.handleChange}
+                  />
+                  <Form.Input 
+                        label="Add Subject" 
+                        action={{icon:"add square"}} 
+                        placeholder="ICS 313" 
+                        type="subject" 
+                        name="subject" 
+                        onClick={this.handleSubjectsClick}
+                  />
+                  {/*
+                    let {subjects} = this.state
+                    this.state.subjects.map((val, i)=>{
+                      return(
+                        <Label key={i}>Test</Label>
+                      )
+                    })
+                    */
+                  }
                   <Form.Button content="Submit"/>
                 </Segment>
               </Form>
