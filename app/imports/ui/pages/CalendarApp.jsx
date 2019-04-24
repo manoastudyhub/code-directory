8a477a4e75bd2d8910ab1cd0814bfffc7646e1c1
Angeli Amascual <aamascua@hawaii.edu>
2019-04-24 00:41:40 -1000

    trying to link sessions

--- app/imports/ui/pages/CalendarApp.jsx
+++ app/imports/ui/pages/CalendarApp.jsx
@@ -1,27 +1,41 @@
 import React from 'react';
+import { Meteor } from 'meteor/meteor';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
+import PropTypes from 'prop-types';
+import { Sessions } from '/imports/api/session/session';
+import { withTracker } from 'meteor/react-meteor-data';
 
 // must manually import the stylesheets for each plugin
 import '@fullcalendar/core/main.css';
 import '@fullcalendar/daygrid/main.css';
 import '@fullcalendar/timegrid/main.css';
 
+
 /** Uses FullCalendar's react component https://fullcalendar.io/docs/react */
 /** Code used is based on FullCalendar's given runnable project https://codesandbox.io/s/2z6wp2jozn */
 
-export default class CalendarApp extends React.Component {
+class CalendarApp extends React.Component {
   calendarComponentRef = React.createRef();
+
+  eventData() {
+    this.props.sessions.map((session) => this.setState({
+      // add new event data
+      calendarEvents: this.state.calendarEvents.concat({
+        // creates a new array
+        title: session.course,
+        start: session.date,
+      }),
+    }));
+  }
 
   state = {
     calendarWeekends: true,
-    calendarEvents: [
-      // initial event data
-      { title: 'Event Now', start: new Date() },
-    ],
-  };
+    calendarEvents: [{}],
+};
+
 
   render() {
     return (
@@ -68,6 +82,23 @@ export default class CalendarApp extends React.Component {
           allDay: arg.allDay,
         }),
       });
+      this.eventData();
     }
   };
 }
+
+/** Require an array of Stuff documents in the props. */
+CalendarApp.propTypes = {
+  sessions: PropTypes.array.isRequired,
+  ready: PropTypes.bool.isRequired,
+};
+
+/** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
+export default withTracker(() => {
+  // Get access to Stuff documents.
+  const subscription = Meteor.subscribe('Sessions');
+  return {
+    sessions: Sessions.find({}).fetch(),
+    ready: subscription.ready(),
+  };
+})(CalendarApp);
